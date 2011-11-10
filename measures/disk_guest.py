aeac176074cb989ffb7809d9b0f3502d350597ed
Woojong Koh <wjngkoh@gmail.com>
2011-11-10 12:52:49 +0900

    Remove a debug message and modify an error message

--- measures/disk_guest.py
+++ measures/disk_guest.py
@@ -11,17 +11,16 @@ def get(guest):
         stats = proxy.disk_io_counters()
     except socket.error as e:
         if e.errno == errno.ECONNREFUSED:
-            print 'error: guest tools need to be installed on guest OS'
+            print 'warning: guest tools need to be installed on %s' % guest.get_name()
             return result
         else:
             raise
 
     for k, v in stats.iteritems():
         stats[k] = int(v)
-    print stats
 
     result['RREQ'] = stats['read_count']
     result['RBYTES'] = stats['read_bytes']
     result['WREQ'] = stats['write_count']
     result['WBYTES'] = stats['write_bytes']
-    return result;
+    return result
