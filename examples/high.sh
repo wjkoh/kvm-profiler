457be1bf580aa290f9094030504a57d0787c66b8
Jeehoon Kang (windmorning@gmail.com) <Jeehoon Kang (windmorning@gmail.com)>
2011-12-09 22:51:31 +0900

    Add examples.

---
+++ examples/high.sh
@@ -0,0 +1,4 @@
+sleep 5
+./resource -r -s 10000 -n 100000000 &
+./resource -r -s 10000 -n 100000000 &
+
---
+++ examples/low.sh
@@ -0,0 +1,4 @@
+sleep 5
+./resource -s 10000 -n 100000000 &
+./resource -s 10000 -n 100000000 &
+
